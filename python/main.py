7a0dad3e90329f9b1ab67b669e7b9e5c472b8297
nerousapps <nervousrvoltage@gmail.com>
2021-05-12 20:24:18 +0200

    Bristol start engine and daemon

--- python/main.py
+++ python/main.py
@@ -127,9 +127,10 @@ class BristolSynth:
     async def rot1_handler(self, data):
         if data:
             self.synth_index = self.synth_index + 1 if self.synth_index < len(self.available_synths)-2 else 0
+            self.menu_line = (self.available_synths[self.synth_index-1], self.available_synths[self.synth_index], self.available_synths[self.synth_index+1])
         else:
             self.synth_index = self.synth_index - 1 if self.synth_index > 0 else len(self.available_synths)-2
-        self.menu_line = (self.available_synths[self.synth_index+1], self.available_synths[self.synth_index], self.available_synths[self.synth_index-1])
+            self.menu_line = (self.available_synths[self.synth_index+1], self.available_synths[self.synth_index], self.available_synths[self.synth_index-1])
         self.screen.draw_menu(self.menu_line)
 
     async def main(self):
