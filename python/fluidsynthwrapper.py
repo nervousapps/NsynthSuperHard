1f5a7e1fef24edb86563c53507e1093b9eb660c8
nerousapps <nervousrvoltage@gmail.com>
2021-05-13 02:17:03 +0200

    Bristol start engine and daemon

--- python/fluidsynthwrapper.py
+++ python/fluidsynthwrapper.py
@@ -41,6 +41,7 @@ class FluidSynthWrapper:
         self.running = False
         self.loading = self.screen.get_loading()
         self.fs = fluidsynth.Synth()
+        self.sfid = "/usr/share/sounds/sf2/FluidR3_GM.sf2"
 
     async def b1_handler(self):
         self.reload = True
@@ -50,7 +51,7 @@ class FluidSynthWrapper:
 
     async def rot1_handler(self, data):
         self.preset_num += 1
-        self.fs.program_select(0, sfid, 0, self.preset_num)
+        self.fs.program_select(0, self.sfid, 0, self.preset_num)
 
     def stop(self):
         self.running = False
@@ -68,7 +69,7 @@ class FluidSynthWrapper:
                 self.screen.draw_text_box(f"FluidSynth")
                 self.fs.start()
                 sfid = self.fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
-                self.fs.program_select(0, sfid, 0, 0)
+                self.fs.program_select(0, self.sfid, 0, 0)
                 while not self.client.get_all_connections(self.client.get_ports(is_midi=True, name_pattern='Arturia', is_output=True)[0]):
                     print(self.client.get_all_connections(self.client.get_ports(is_midi=True, name_pattern='Arturia', is_output=True)[0]))
                     try:
