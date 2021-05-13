528102db018e0504942e71c0591df8a4dd44b3a3
nerousapps <nervousrvoltage@gmail.com>
2021-05-13 13:20:51 +0200

    Bristol start engine and daemon

--- python/fluidsynthwrapper.py
+++ python/fluidsynthwrapper.py
@@ -40,11 +40,9 @@ class FluidSynthWrapper:
         self.reload = False
         self.running = False
         self.loading = self.screen.get_loading()
-        self.fs = fluidsynth.Synth(2, 48000, 16, [("audio.jack.autoconnect", 1), ("midi.autoconnect", 1)])
-        # self.fs.setting('audio.jack.autoconnect', 1)
-        print(f"########### {self.fs.get_setting('audio.jack.autoconnect')}")
-        # self.fs.setting('midi.autoconnect', 1)
-        print(f"########### {self.fs.get_setting('midi.autoconnect')}")
+        self.fs = fluidsynth.Synth(2, 48000, 16)
+        self.fs.setting('audio.jack.autoconnect', 1)
+        self.fs.setting('midi.autoconnect', 1)
         self.sfid = None
 
     async def b1_handler(self):
